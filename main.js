5ab176b4cfc1059ab721b038b257e24437b6648f
Aafaq hassan <75435702+Aafaqhassan@users.noreply.github.com>
2020-12-26 01:41:25 -0600

    Add files via upload
    this is a work of javascript

---
+++ main.js
@@ -0,0 +1,40 @@
+console.log("Hello Aafaq")
+
+var age 
+var senior=false;
+var young=false;
+var child=false;
+
+
+
+age=21;
+ if (age>=60){
+
+console.log( "you are a senior")
+console.log("you are eligible for visa")
+senior =true;
+
+
+
+} 
+else if (age>=20){
+ console.log("you are young")
+console.log("you are eligible for visa")
+young=true;
+
+
+}
+else if(age>=1){
+
+console.log("You are a child")
+console.log("you are not eligible for visa")
+child =true;
+
+
+
+}
+let a=5;
+let b=6;
+let c=a+b;
+let d=a-b;
+console.log(d);
